056efc6246e59ad7bc99da42eca8e248e9d21ee6
Nabila Khansa <nabila.khansa@gmail.com>
2023-10-21 16:39:56 +0700

    default empty line

--- double_lobby_lb.py
+++ double_lobby_lb.py
@@ -56,6 +56,7 @@ def get_penalty_table():
 
 |  Game  | Team Name | Penalty | Reason                |
 |:-------|:----------|:--------|:----------------------|
+|        |           |         |                       |
     """
     return penalty
 
